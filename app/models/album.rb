39f35507d1f0d4266769296081191d161eff9c51
TheYearIsTwentyGX <twentygx@outlook.com>
2023-08-30 14:13:13 -0500

    Added validations

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,8 @@
 class Album < ApplicationRecord
+	validates :Title, presence: true, uniqueness: true
+	validates :TrackCount, presence: true, numericality: { only_integer: true }
+	validates :Artist, presence: true
+
+	has_many :reviews
+	has_many :reviewers, through: :reviews, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :password, presence: true, length: { minimum: 4 }, allow_nil: true
+  validates :Username, presence: true, uniqueness: true
 end
