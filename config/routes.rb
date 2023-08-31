b23ceaade9c745e4503f9d121468ccef2e74b893
TheYearIsTwentyGX <twentygx@outlook.com>
2023-08-31 16:56:14 -0500

    Modified paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
+  get "/session", to: "sessions#index"
   resources :reviews
   resources :albums
   resources :users
   post "/login", to: "sessions#create"
-  post "/logout", to: "sessions#destroy"
   post "/signup", to: "users#create"
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  post "/logout", to: "sessions#destroy"
 end
