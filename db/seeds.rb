c02bc4b860d23a4bb5d57f18482a04177ad98a4e
TheYearIsTwentyGX <twentygx@outlook.com>
2023-09-05 11:57:11 -0500

    Added more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 Album.create(Artist: "KiNG MALA", Title: "honey catching season", TrackCount: 9, AlbumArt: "https://th.bing.com/th/id/OIP.FWn3zFVjTpECnCKP1LLqmgAAAA?pid=ImgDet&rs=1")
+Album.create(Artist: "UPSAHL", Title: "Lady Jesus", TrackCount: 10, AlbumArt: "https://t2.genius.com/unsafe/860x0/https%3A%2F%2Fimages.genius.com%2Fd8603c4b3dd5ce842c29712f68d7556c.640x640x1.jpg")
 User.create!(Username: "DylanH", password: "6988", password_confirmation: "6988")
 User.create!(Username: "admin", password: "admin", password_confirmation: "admin")
-Review.create!(Title: "This is my favorite album!", Body: "Her best stuff.", user_id: 1, album_id: 1)
+Review.create!(Title: "This is my favorite album!", Body: "Her best stuff. I got a tattoo based off her merch from this album :D", user_id: 1, album_id: 1)
+Review.create!(Title: "This album in 2 words: Delusional Confidence", Body: "So much of this album is about just gaslighting yourself into thinking everything is going great. Thriving is a great example of this", user_id: 1, album_id: 2)
